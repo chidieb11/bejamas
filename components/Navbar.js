afdaef538f4510c34ec28585c74caab4074ab28e
Chidiebere <chukwuchidieberejohn@gmail.com>
2022-06-11 09:49:54 +0100

    Sticky Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,19 @@
-import {AiOutlineShoppingCart} from "react-icons/ai";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 const Navbar = () => {
-    return (
-        <>
-            <div>
-                <div
-                    className="flex top-0 sticky justify-between items-center mt-4 sm:m-5 text-4xl sm:mt-2 sm:mr-20 sm:ml-20">
-                    <div className="cursor-pointer font-bold">Bejamaz</div>
-                    <div className="cursor-pointer"><AiOutlineShoppingCart/></div>
-                </div>
-                <hr className="bg-gray-100 h-1 mt-5 sm:mr-20 sm:ml-20"/>
-            </div>
-        </>
-
-    );
+  return (
+    <>
+      <div className="top-0 sticky z-10">
+        <div className="flex justify-between items-center mt-4 sm:m-5 text-4xl sm:mt-2 sm:mr-20 sm:ml-20">
+          <div className="cursor-pointer font-bold">Bejamaz</div>
+          <div className="cursor-pointer">
+            <AiOutlineShoppingCart />
+          </div>
+        </div>
+        <hr className="bg-gray-100 h-1 mt-5 sm:mr-20 sm:ml-20" />
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
